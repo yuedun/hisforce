4d224eb9ef6c33c1090b643f51aa71a7ad9bd5fa
yuedun <light_v@163.com>
2015-12-13 00:13:01 +0800

    异步请求数据vue-resource

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
 var Vue = require('./vue.js');
 var VueRouter = require('./vue-router.min.js');
 Vue.use(VueRouter);
+var VueResource = require('./vue-resource.min');
+Vue.use(VueResource);
 // 定义组件
 var Foo = Vue.extend({
-	template: '<div class="foo">' + '<h2>This is Foo!</h2>' + '<router-view></router-view>' + // <- 嵌套的外链
+	template: '<div class="foo">' + 
+	'<h2>This is Foo!</h2>' + 
+	'<router-view></router-view>' + // <- 嵌套的外链
 	'</div>'
-})
-
-var Bar = Vue.extend({
-	template: "<p>Bar</p>"
-})
-var Baz = Vue.extend({
-	template: '<p>This is Baz!</p>'
-})
+});
 
 // 路由器需要一个根组件。
 // 出于演示的目的，这里使用一个空的组件，直接使用 HTML 作为应用的模板
-var App = Vue.extend({})
+var App = Vue.extend({});
 
 // 创建一个路由器实例
 // 创建实例时可以传入配置参数进行定制，为保持简单，这里使用默认配置
-var router = new VueRouter()
+var router = new VueRouter();
 
 // 定义路由规则
 // 每条路由规则应该映射到一个组件。这里的“组件”可以是一个使用 Vue.extend
@@ -38,12 +35,12 @@ router.map({
 			},
 			'/baz': {
 				// Baz也是一样，不同之处是匹配的路由会是/foo/baz
-				component: Baz
+				component: require('./list.vue')
 			}
 		}
 	}
-})
+});
 
 // 现在我们可以启动应用了！
 // 路由器会创建一个 App 实例，并且挂载到选择符 #app 匹配的元素上。
-router.start(App, '#app')
+router.start(App, '#app');
---
+++ list.vue
@@ -0,0 +1,35 @@
+<style>
+	
+</style>
+<template>
+	<h1>获取一个列表</h1>
+	<div id="app3">
+	  <button v-on:click="getList">获取</button>
+	  <ul>
+	    <li v-for="todo in todos">
+	      <span>{{{ todo.title }}}</span>
+	    </li>
+	  </ul>
+	</div>
+</template>
+<script>
+	module.exports = {
+		data: function(){
+			return {
+			    todos: []
+			}
+		},
+		methods: {
+		    getList: function () {
+                this.$http.get('https://cnodejs.org/api/v1/topics', function (data, status, request) {
+                	console.log(">>>>>>>>>>>>>success");
+                	//console.log(">>>>>>>>>>>>>success"+JSON.stringify(data));
+                	this.todos = data.data;
+                }).error(function (data, status, request) {
+                	console.log(">>>>>>>>>>err"+data);
+			        // handle error
+				});
+		    }
+		}
+	}
+</script>
